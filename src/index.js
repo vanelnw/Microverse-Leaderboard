7e98087639fd4842190713751eab2b57f37e33f5
vanelnw <vanelnwaba@gmail.com>
2022-12-23 07:27:46 +0100

    Simplify Event Listener’s  for better performance

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import './style.css';
 
 const leaderboard = new Leaderboard();
 
-const scoreContent = document.querySelector('.score-content');
-const form = document.getElementById('form');
-const nameInput = document.getElementById('name');
-const scoreInput = document.getElementById('score');
-const refreshBtn = document.getElementById('refresh');
+const scoreContent = document.querySelector('.score-list');
+const form = document.getElementById('add-score-form');
+const nameInput = document.getElementById('name-input');
+const scoreInput = document.getElementById('score-input');
+const refreshBtn = document.getElementById('refresh-button');
 
-document.addEventListener('DOMContentLoaded', leaderboard.refreshScores());
+const refreshScores = () => {
+  scoreContent.innerHTML = '';
+  leaderboard.refreshScores();
+};
 
-form.addEventListener('submit', (event) => {
+const handleFormSubmit = (event) => {
   event.preventDefault();
   leaderboard.addScore(new FormData(form));
   nameInput.value = '';
   scoreInput.value = '';
-});
+};
 
-refreshBtn.addEventListener('click', () => {
-  scoreContent.innerHTML = '';
-  leaderboard.refreshScores();
-});
+document.addEventListener('DOMContentLoaded', refreshScores);
+
+form.addEventListener('submit', handleFormSubmit);
+
+refreshBtn.addEventListener('click', refreshScores);
